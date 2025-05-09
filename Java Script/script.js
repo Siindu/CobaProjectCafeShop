9ec7739432c0585d9b34d0acc6041ba195fbb42c
Siindu <ardianasindu@gmail.com>
2025-05-09 22:54:34 +0800

    Ubah totall

---
+++ Java Script/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// PRELOAD 
+
+const preloader = document.querySelector("[data-preload]");
+
+window.addEventListener("load", function () {
+    preloader.classList.add("loaded");
+    this.document.body.classList.add("loaded");
+})
